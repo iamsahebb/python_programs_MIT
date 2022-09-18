992c204c0f9b30356a3b7dfd151f1849b6aefbd3
mainak-wq <mainakdeb1998@gmail.com>
2022-09-18 12:02:38 +0530

    mainak's siliguri commit

---
+++ test.py
@@ -0,0 +1,9 @@
+class Test:
+	def main():
+		a=str(input("Enter your first name -> "))
+		b= str(input("Enter your Surname name -> "))
+		c= "Mr."+" "+a+" "+b
+		print("Your name is -> ",c)
+	main()
+obj_1=Test
+obj_1.main
